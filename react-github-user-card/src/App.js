844b7182dabb52fce264b039095a6f28489c158a
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-06 12:18:50 -0400

    Added axios and data component

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Data from "./components/data";
 
 function App() {
   return <div className="App" />;
---
+++ react-github-user-card/src/components/data.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Data extends Component {
+  componentDidMount() {
+    axios;
+  }
+  render() {
+    return <div />;
+  }
+}
